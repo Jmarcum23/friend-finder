4500398638892f3813bfa51f9dad9aad95bd2db8
Jennifer Marcum <Jen@jennifermarcum.com>
2017-11-24 20:41:03 -0800

    Updated friends JS

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Pixel Marcum',
+    profilePic: 'http://www.jennifermarcum.com/xwp/wp-content/uploads/about-img-10.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
